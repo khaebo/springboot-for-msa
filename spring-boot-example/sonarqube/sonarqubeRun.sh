cb0dca2bd5d5db6f7636597d30594abde8d4e68e
haebo <khaebo@gmail.com>
2023-05-07 09:25:16 +0900

    sonarqube 쉘 푸시

---
+++ spring-boot-example/sonarqube/sonarqubeRun.sh
@@ -0,0 +1,5 @@
+mvn sonar:sonar \
+-Dsonar.projectKey=springboot-for-msa \
+-Dsonar.projectName=springboot-for-msa \
+-Dsonar.host.url=https://sonarqube.haebo.me \
+-Dsonar.token=sqa_9363b7e52dbb77a910105dd89226afb926166436
